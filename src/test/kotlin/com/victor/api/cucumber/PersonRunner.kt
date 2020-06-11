2a7c924de8daf8e7a1fd5a38ce94d097a1244d7a
Victor Yuri Alves Tripeno <victoryuri91@hotmail.com>
2020-06-11 20:27:00 -0300

    Add cucumber to serenity report

--- src/test/kotlin/com/victor/api/cucumber/PersonRunner.kt
+++ src/test/kotlin/com/victor/api/cucumber/PersonRunner.kt
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith
 
 @RunWith(CucumberWithSerenity::class)
 @CucumberOptions(
+        strict = true,
         plugin = ["pretty", "html:target/cucumber-html-report", "junit:target/junit-report.xml"],
+        monochrome = false,
         glue = ["com.victor.api.cucumber"],
         features = ["classpath:features"]
 )
